783e9890b59dbbc67f5d22eec27504b33f26f197
Artsemi <artemij1256@gmail.com>
2023-11-21 21:34:25 +0100

    added start data if doctors table is empty (head physician)

---
+++ Hospital/Models/SeedData.cs
@@ -0,0 +1,34 @@
+﻿using Hospital.Data;
+using Microsoft.EntityFrameworkCore;
+
+namespace Hospital.Models
+{
+    public static class SeedData
+    {
+        public static void Initialize(IServiceProvider serviceProvider)
+        {
+            using (var context = new HospitalContext(
+                serviceProvider.GetRequiredService<
+                    DbContextOptions<HospitalContext>>()))
+            {
+                // Look for any movies.
+                if (context.Doctor.Any())
+                {
+                    return;   // DB has been seeded
+                }
+
+                context.Doctor.AddRange(
+                    new Doctor
+                    {
+                        Name = "John Smith",
+                        Login = "johnsmith",
+                        Password = "12345678",
+                        Specialty = Spec.FamilyDoctor,
+                        IsAdmin = true
+                    }
+                );
+                context.SaveChanges();
+            }
+        }
+    }
+}
--- Hospital/Program.cs
+++ Hospital/Program.cs
@@ -1,6 +1,7 @@
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
 using Hospital.Data;
+using Hospital.Models;
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddDbContext<HospitalContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("HospitalContext") ?? throw new InvalidOperationException("Connection string 'HospitalContext' not found.")));
@@ -9,6 +10,13 @@ builder.Services.AddDbContext<HospitalContext>(options =>
 builder.Services.AddControllersWithViews();
 
 var app = builder.Build();
+
+using (var scope = app.Services.CreateScope())
+{
+    var services = scope.ServiceProvider;
+
+    SeedData.Initialize(services);
+}
 
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
